da8c16cd3116cd3bae05f9c9dfc2385b3d274d1c
Carlos Gasperi <cgasperi90@gmail.com>
2020-02-24 14:31:18 -0500

    fixing javascript code to reset counter of guesses whenever you win or lose.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,7 +5,7 @@
 var wins = 0;
 var losses = 0;
 var guessesLeftSoFar = 9;
-var guessesArray = "";
+var guessesArray = [];
 
 //variables for elements inside index
 var winsText = document.getElementById("wins-text");
@@ -13,14 +13,20 @@ var lossesText = document.getElementById("losses-text");
 var guessesLeft = document.getElementById("guesses-left");
 var guessesSoFar = document.getElementById("guesses-sofar");
 var userChoice = document.getElementById("user-choice");
+var directions = document.getElementById("directions");
 
 
 //the variable to hold the choices that the computer can choose from
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y"];
 
+function reset() {
+    guessesLeftSoFar = 9;
+}
 
 //my function for the event
 document.onkeyup = function(event) {
+
+    guessesArray.push(userGuess);
     
     var userGuess = event.key.toLowerCase();
 
@@ -29,13 +35,15 @@ document.onkeyup = function(event) {
     if (userGuess === computerGuess) {
         wins++;
         alert("You Guessed Right!!");
+        reset();
     } else if (userGuess !== computerGuess) {
         guessesLeftSoFar--;
-    } else {
-        guessesArray.push(userGuess);
+    } if (guessesLeftSoFar === 0) {
+        losses++;
+        reset();
     }
 
-
+    directions.textContent = "";
 
     userChoice.textContent = "You chose: " + userGuess;
     guessesLeft.textContent = "Guesses left: " + guessesLeftSoFar;
